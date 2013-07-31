cdfc5f47c0f22970e898b056ebd98f4a195dccb0
rowend <rowend07@gmail.com>
2013-07-31 08:39:18 -0500

    productos

--- upload/admin/language/spanish/amazonus/products.php
+++ upload/admin/language/spanish/amazonus/products.php
@@ -1,54 +1,54 @@
 <?php
 
-$_['heading_title'] = 'Amazon US listing';
+$_['heading_title'] = 'Listas de Amazon US';
 
 /* Headers, tab names */
-$_['item_links_header_text'] = 'Item links';
-$_['quick_listing_header_text'] = 'Quick listing';
-$_['advanced_listing_header_text'] = 'Advanced listing';
-$_['saved_heder_text'] = 'Saved listings';
+$_['item_links_header_text'] = 'Enlaces del artículo';
+$_['quick_listing_header_text'] = 'Listado rapido';
+$_['advanced_listing_header_text'] = 'Listado Avanzado';
+$_['saved_heder_text'] = 'Listas guardadas';
 
 
-$_['item_links_tab_text'] = 'Item links';
-$_['quick_listing_tab_text'] = 'Quick listing';
-$_['advanced_listing_tab_text'] = 'Advanced listing';
-$_['saved_tab_text'] = 'Saved listings';
+$_['item_links_tab_text'] = 'Enlaces del artículo';
+$_['quick_listing_tab_text'] = 'Listadp rapido';
+$_['advanced_listing_tab_text'] = 'Listado avanzado';
+$_['saved_tab_text'] = 'Listados guaradados';
 
-$_['text_error_connecting'] = 'Warning: There was problem connecting to Welford Media API servers. Please check your OpenBay Pro Amazon US extension settings. If the problem persists, please contact Welford support.';
+$_['text_error_connecting'] = 'Cuidado: Hubo un problem conectando al APi de  Welford Media. Por favor checa tus configuraciones en la exntensión de OpenBay Pro Amazon US. Si el problema persiste, contacta el soporte de Welford.';
 
 /* Quick/advanced listing tabs */
-$_['quick_listing_description'] = 'Use this method when matching product already exist in Amazon catalogue. Matching is found by Amazon using Standard product id (ASIN, ISBN, UPS, EAN)';
-$_['advanced_listing_description'] = 'Use this method to create new listings on Amazon US.';
-$_['listing_row_text'] = 'Listing for product:';
-$_['already_saved_text'] = 'This product is already in saved listings. Click edit if you want to revise.';
-$_['save_button_text'] = 'Save';
-$_['cancel_button_text'] = 'Cancel';
-$_['field_required_text'] = 'This field is required!';
-$_['not_saved_text'] = 'Listing was not saved. Check your input.';
-$_['chars_over_limit_text'] = 'characters over limit.';
-$_['minimum_length_text'] = 'Minimum length is';
-$_['characters_text'] = 'characters';
-$_['delete_confirm_text'] = 'Are you sure?';
+$_['quick_listing_description'] = 'Usar este metodo cuando un producto que coincida ya exista en el catalogo de Amazon. Las coincidencias son encontradas por Amazon usando el ID estandar del producto(ASIN, ISBN, UPS, EAN)';
+$_['advanced_listing_description'] = 'Usar este metodo para crear nuevas listas en Amazon US.';
+$_['listing_row_text'] = 'Listar por producto:';
+$_['already_saved_text'] = 'Este producto ya ha sido guardado en las listas. Click en editar si quieres hacer revisiones.';
+$_['save_button_text'] = 'Guardar';
+$_['cancel_button_text'] = 'Cancelar';
+$_['field_required_text'] = 'Este campo es obligatorio!';
+$_['not_saved_text'] = 'Lista no guardada. Revisa tus entradas.';
+$_['chars_over_limit_text'] = 'Caracteres sobre el limite.';
+$_['minimum_length_text'] = 'La longitud minima es';
+$_['characters_text'] = 'Caracteres';
+$_['delete_confirm_text'] = '¿Estas seguro?';
 
-$_['clear_image_text'] = 'Clear';
-$_['browse_image_text'] = 'Browse';
+$_['clear_image_text'] = 'Limpiar';
+$_['browse_image_text'] = 'Buscar';
 
-$_['category_selector_field_text'] = 'Amazon category:';
+$_['category_selector_field_text'] = 'Categoria de Amazon:';
 
 /* Item links tab */
-$_['item_links_description'] = 'Here you can add and edit product links to already existing Amazon items without listing them from OpenCart. This will allow stock control between enabled marketplaces. If you have openStock installed - this will also allow you to link item options to individual Amazon SKUs. (Uploading products from OpenCart to Amazon will automaticaly add links)';
-$_['new_link_table_name'] = 'New link'; 
-$_['new_link_product_column'] = 'Product';
+$_['item_links_description'] = 'Aqui tu puedes agregar y editar los enlaces de los productos que ya existen en Amazon, artículos que no estan listados  en OpenCart. Esto te permitira el control del stock entre mercados habilitados. Si tienes instalado openStock - Te permitira enlazar las opciones de los artículos de manera individual a los SKUs de Amazon. (Subir producto desde OpenCart hacia Amazon automaticamente agrega enlaces)';
+$_['new_link_table_name'] = 'Nuevo enlace'; 
+$_['new_link_product_column'] = 'Producto';
 $_['new_link_sku_column'] = 'SKU';
-$_['new_link_amazonus_sku_column'] = 'Amazon item SKU';
-$_['new_link_action_column'] = 'Action';
+$_['new_link_amazonus_sku_column'] = 'SKU del artículo de Amazon';
+$_['new_link_action_column'] = 'Acción';
 
-$_['item_links_table_name'] = 'Item links'; 
+$_['item_links_table_name'] = 'Enlaces del Artículo'; 
 
 
 /* Marketplaces */
-$_['marketplaces_field_text'] = 'Marketplace';
-$_['marketplaces_help'] = 'You can choose default marketplace in Amazon US extension settings.';
+$_['marketplaces_field_text'] = 'Mercado';
+$_['marketplaces_help'] = 'Puedes seleccionar un mercado por defualt en las configuraciones de la extensión de Amazon US.';
 
 
 /* Saved listings tab */
@@ -61,28 +61,28 @@ $_['name_column_text'] = 'Name';
 $_['model_column_text'] = 'Model';
 $_['sku_column_text'] = 'SKU';
 $_['amazonus_sku_column_text'] = 'Amazon item SKU';
-$_['actions_column_text'] = 'Action';
-$_['uploaded_alert_text'] = 'Saved listing(s) uploaded!';
+$_['actions_column_text'] = 'Acción';
+$_['uploaded_alert_text'] = 'Lista(s) guardada(s) subida(s)!';
 
 
 /* ITEM LINKS */
-$_['links_header_text'] = 'Link items';
-$_['links_desc1_text'] = 'Linking your items will allow for stock control on your Amazon US listings.<br /> For each item that is updated the local stock (the stock available in your OpenCart store) will update your Amazon listing';
-$_['links_desc2_text'] = 'You can link items manualy by entering Amazon SKU and product name or load all unlinked products and then enter Amazon SKUs. (Uploading products from OpenCart to Amazon will automaticaly add links)';
-$_['links_load_btn_text'] = 'Load';
-$_['links_new_link_text'] = 'New link';
-$_['links_autocomplete_product_text'] = 'Product<span class="help">(Autocomplete from name)</span>';
-$_['links_amazonus_sku_text'] = 'Amazon item SKU';
-$_['links_action_text'] = 'Action';
-$_['links_add_text'] = 'Add';
-$_['links_add_sku_tooltip'] = 'Add another sku';
-$_['links_remove_text'] = 'Remove';
-$_['links_linked_items_text'] = 'Linked items';
-$_['links_unlinked_items_text'] = 'Unlinked items';
-$_['links_name_text'] = 'Name';
-$_['links_model_text'] = 'Model';
+$_['links_header_text'] = 'Artículos del Enlace';
+$_['links_desc1_text'] = 'Enlazando tus artículos te permitira controlar tu stock en las listas de Amazon US.<br /> Por cada artículo que es actualizado el stock local (the stock disponible en tu OpenCart) automaticamente actualizara tus listas de Amazon';
+$_['links_desc2_text'] = 'Puedes enlazar artículos manualmente introduciendo Amazon SKU y el nombre del producto o cargar todos los productos no enlazados e introducir los Amazon SKUs. (Subir productos con OpenCart hacia Amazon automaticamente agrega enlaces)';
+$_['links_load_btn_text'] = 'Cargar';
+$_['links_new_link_text'] = 'Nuevo enlace';
+$_['links_autocomplete_product_text'] = 'Product0<span class="help">(Autocompleta con el nombre)</span>';
+$_['links_amazonus_sku_text'] = 'SKU del artículo de Amazon';
+$_['links_action_text'] = 'Acción';
+$_['links_add_text'] = 'Agregar';
+$_['links_add_sku_tooltip'] = 'Agregar otro SKU';
+$_['links_remove_text'] = 'Remover';
+$_['links_linked_items_text'] = 'Artículos enlazados';
+$_['links_unlinked_items_text'] = 'Artículos no enlazados';
+$_['links_name_text'] = 'Nombre';
+$_['links_model_text'] = 'Modelo';
 $_['links_sku_text'] = 'SKU';
-$_['links_amazonus_sku_text'] = 'Amazon item SKU';
-$_['links_sku_empty_warning'] = 'Amazon SKU can not be empty!';
-$_['links_name_empty_warning'] = 'Product name can not be empty!';
-$_['links_product_warning'] = 'Product does not exist. Please use autocomplete values.';
+$_['links_amazonus_sku_text'] = 'SKU del artículo de Amazon';
+$_['links_sku_empty_warning'] = 'El SKU no puede estar vacio!';
+$_['links_name_empty_warning'] = 'El nombre del producto no puede estar vacio!';
+$_['links_product_warning'] = 'El producto no existe. Por favor usar los valores del autocompletado.';
