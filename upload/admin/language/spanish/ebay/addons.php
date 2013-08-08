38fa839a058b7b09ef1340be40d3f4aff693d7be
rowend <rowend07@gmail.com>
2013-08-08 11:00:04 -0500

    addons file

--- upload/admin/language/spanish/ebay/addons.php
+++ upload/admin/language/spanish/ebay/addons.php
@@ -1,12 +1,12 @@
 <?php
 $_['lang_openbay']              = 'OpenBay Pro';
-$_['lang_page_title']           = 'OpenBay Pro for eBay';
+$_['lang_page_title']           = 'OpenBay Pro para eBay';
 $_['lang_ebay']                 = 'eBay';
-$_['lang_heading']              = 'Addons';
-$_['lang_addon_desc']           = 'Addons are modules that are known to be supported or enhance the usability of OpenBay Pro.';
-$_['lang_addon_name']           = 'Addon name';
-$_['lang_addon_version']        = 'Version';
-$_['lang_addon_none']           = 'You have no addons installed';
-$_['lang_error_validation']     = 'You need to register for your API token and enable the module.';
-$_['lang_btn_return']           = 'Return';
+$_['lang_heading']              = 'Complementos';
+$_['lang_addon_desc']           = 'Complementos son modulos que son conocidos  por soportar o mejorar la usabilidad de OpenBay Pro.';
+$_['lang_addon_name']           = 'Nombre del complemento';
+$_['lang_addon_version']        = 'Versión';
+$_['lang_addon_none']           = 'No tienes complementos instalados';
+$_['lang_error_validation']     = 'Usted necesita registrar su API token y habilitar el modulo.';
+$_['lang_btn_return']           = 'Regresar';
 
