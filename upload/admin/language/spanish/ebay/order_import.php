b8d51478fa1968eb8249a9197dfb2f8e1e4440d1
rowend <rowend07@gmail.com>
2013-08-12 11:04:07 -0500

    order_import

--- upload/admin/language/spanish/ebay/order_import.php
+++ upload/admin/language/spanish/ebay/order_import.php
@@ -1,13 +1,13 @@
 <?php
 $_['lang_openbay']                      = 'OpenBay Pro';
-$_['lang_page_title']                   = 'OpenBay Pro for eBay';
+$_['lang_page_title']                   = 'OpenBay Pro para eBay';
 $_['lang_ebay']                         = 'eBay';
-$_['lang_heading']                      = 'Order import';
-$_['lang_btn_return']                   = 'Return';
-$_['lang_sync_orders']                  = 'Orders';
-$_['lang_sync_pull_orders']             = 'Pull new orders';
-$_['lang_sync_pull_orders_text']        = 'Pull orders';
-$_['lang_ajax_load_error']              = 'Sorry, could not get a connection';
-$_['lang_error_validation']             = 'You need to register for your API token and enable the module.';
-$_['lang_sync_pull_notice']             = 'This will pull new orders since the last automated check. If you have just installed then it will default to the last 24 hours.';
-$_['lang_ajax_orders_import']           = 'Any new orders should appear within a few minutes';
+$_['lang_heading']                      = 'Importancia del Pedido';
+$_['lang_btn_return']                   = 'Devolución';
+$_['lang_sync_orders']                  = 'Pedidos';
+$_['lang_sync_pull_orders']             = 'Tirar nuevos pedidos';
+$_['lang_sync_pull_orders_text']        = 'Tirar Pedidos';
+$_['lang_ajax_load_error']              = 'Perdon, NO pudimos obtener una conección';
+$_['lang_error_validation']             = 'Tenecistas registrar tu API del token y habilitar el modulo.';
+$_['lang_sync_pull_notice']             = 'Esto jalara nuevos pedidos desde el ultimo chequeo automatizado. Si tu solo lo has instalado sera activado por defualt dentro de 24 horas.';
+$_['lang_ajax_orders_import']           = 'Cualquier pedido nuevo, debería aparecer dentro de un par de minutos';
