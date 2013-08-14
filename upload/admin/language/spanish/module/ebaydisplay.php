e54ccefbc8fe595f23e3b91c2e207defafc37770
rowend <rowend07@gmail.com>
2013-08-14 10:18:15 -0500

    ebaydisplay

--- upload/admin/language/spanish/module/ebaydisplay.php
+++ upload/admin/language/spanish/module/ebaydisplay.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']       = 'eBay display';
+$_['heading_title']       = 'Dispocisión de eBay';
 
 // Text
-$_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module eBay featured!';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
+$_['text_module']         = 'Modulos';
+$_['text_success']        = 'Exito: Has modificado el modulo de eBay!';
+$_['text_content_top']    = 'Contenido Superior';
+$_['text_content_bottom'] = 'Contenido Inferior';
+$_['text_column_left']    = 'Columna Izquierda';
+$_['text_column_right']   = 'Column Derecha';
 
 // Entry
-$_['entry_product']       = 'Products:<br /><span class="help">(Autocomplete)</span>';
-$_['entry_limit']         = 'Limit:';
-$_['entry_image']         = 'Image (W x H) and Resize Type:';
-$_['entry_layout']        = 'Layout:';
-$_['entry_position']      = 'Position:';
-$_['entry_status']        = 'Status:';
-$_['entry_sort_order']    = 'Sort Order:';
+$_['entry_product']       = 'Productos:<br /><span class="help">(Autocompletar)</span>';
+$_['entry_limit']         = 'Limite:';
+$_['entry_image']         = 'Imagen (A x A) y tipo de tamaño:';
+$_['entry_layout']        = 'Disposición:';
+$_['entry_position']      = 'Posición:';
+$_['entry_status']        = 'Estatus:';
+$_['entry_sort_order']    = 'Orden de Aparición:';
 
 // Error 
-$_['error_permission']    = 'Warning: You do not have permission to modify module featured!';
-$_['error_image']         = 'Image width &amp; height dimensions required!';
+$_['error_permission']    = 'Cuidado: No tienes permisos para modificar el modulo!';
+$_['error_image']         = 'Las dimensiones ancho &amp; alto de la Imagen son obligatorias!';
 ?>
