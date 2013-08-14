96e8bbf13cb62947450bf7f0c682790b11735dd6
rowend <rowend07@gmail.com>
2013-08-14 10:21:24 -0500

    pp_layout

--- upload/admin/language/spanish/module/pp_layout.php
+++ upload/admin/language/spanish/module/pp_layout.php
@@ -1,18 +1,18 @@
 <?php
 
 // Heading
-$_['heading_title'] = 'PayPal Express Checkout button';
+$_['heading_title'] = 'Boton de PayPal Express Checkout';
 
-$_['text_module'] = 'Modules';
-$_['text_success'] = 'Success: You have modified module PP Layout!';
-$_['text_content_top'] = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left'] = 'Column Left';
-$_['text_column_right'] = 'Column Right';
+$_['text_module'] = 'Modulos';
+$_['text_success'] = 'Exito: Has modificado el modulo de PP Layout!';
+$_['text_content_top'] = 'Contenido Superior';
+$_['text_content_bottom'] = 'Content Inferior';
+$_['text_column_left'] = 'Columna Izquierda';
+$_['text_column_right'] = 'Columna Derecha';
 
-$_['entry_layout'] = 'Layout:';
-$_['entry_position'] = 'Position:';
-$_['entry_status'] = 'Status:';
-$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_layout'] = 'Posición:';
+$_['entry_position'] = 'Posición:';
+$_['entry_status'] = 'Estatus:';
+$_['entry_sort_order'] = 'Orden de Aparición:';
 
-$_['error_permission'] = 'Warning: You do not have permission to modify module PP Layout!';
+$_['error_permission'] = 'Cuidado: No tienes permisos para modificar el modulo de PP Layout!';
