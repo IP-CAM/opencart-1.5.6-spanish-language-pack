461d4d83348e9c40e258c7153363debf229eabf4
rowend <rowend07@gmail.com>
2013-08-15 10:03:11 -0500

    openbay

--- upload/admin/language/spanish/openbay/openbay.php
+++ upload/admin/language/spanish/openbay/openbay.php
@@ -1,9 +1,9 @@
 <?php 
 $_['heading_title']                     = 'eBay';
-$_['text_edit']                         = 'Edit';
-$_['text_install']                      = 'Install';
-$_['text_uninstall']                    = 'Uninstall';
-$_['text_enabled']                      = 'Enabled';
-$_['text_disabled']                     = 'Disabled';
-$_['error_category_nosuggestions']      = 'Could not load any suggested categories';
-$_['lang_text_success']                 = 'You have saved your changes to the eBay extension';
+$_['text_edit']                         = 'Editar';
+$_['text_install']                      = 'Instalar';
+$_['text_uninstall']                    = 'Desinstalar';
+$_['text_enabled']                      = 'Habilitar';
+$_['text_disabled']                     = 'No habilitar';
+$_['error_category_nosuggestions']      = 'No se pudieron cargar las categorías sugeridas';
+$_['lang_text_success']                 = 'Has guardado tus cambios en la extensión de eBay';
