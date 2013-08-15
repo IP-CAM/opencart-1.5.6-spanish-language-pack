1f840fe440bdcc239d59a52c5886b3daeb71c813
rowend <rowend07@gmail.com>
2013-08-15 09:58:16 -0500

    play file

--- upload/admin/language/spanish/openbay/play.php
+++ upload/admin/language/spanish/openbay/play.php
@@ -1,4 +1,4 @@
 <?php
 $_['heading_title']         = 'Play.com (EU)';
-$_['lang_heading_title']    = 'OpenBay Pro for Play.com';
+$_['lang_heading_title']    = 'OpenBay Pro para Play.com';
 /* Settings tab */
