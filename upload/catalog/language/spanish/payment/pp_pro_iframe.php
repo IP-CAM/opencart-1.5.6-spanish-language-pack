744e072078a6c4eab94fb7e6a401d88e4878a2b7
rowend <rowend07@gmail.com>
2013-08-28 09:31:49 -0500

    pp

--- upload/catalog/language/spanish/payment/pp_pro_iframe.php
+++ upload/catalog/language/spanish/payment/pp_pro_iframe.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_title'] = 'Credit or Debit Card';
-$_['text_secure_connection'] = 'Creating a secure connection...';
+$_['text_title'] = 'Tarjeta de credito o Debito';
+$_['text_secure_connection'] = 'Creando una conexión segura...';
 
 //Errors
-$_['error_connection'] = "Could not connect to PayPal. Please contact the shop's administrator for assistance or choose a different payment method.";
+$_['error_connection'] = "No pudimos conectar con PayPal. Por favor contactar con el administrador para asistencia o para obterner un metodo diferente de pago.";
 ?>
